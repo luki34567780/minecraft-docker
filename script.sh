40050bba7b58f7cc159714385f2558944fba9805
Lukas Seitz <lukas-sinzing@gmx.de>
2021-12-21 19:03:35 +0100

    agreeing to eula automaticly

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 echo "WARNING! Using a early version of PaperMC 1.18.1 which may contain bugs"
 chmod 777 /data
 cd /data
+echo eula=true > /data/eula.txt
 sudo -u server java -Xmx2048M -Xms2048M -jar /jar/server.jar nogui
